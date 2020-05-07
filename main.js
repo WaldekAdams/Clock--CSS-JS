9d2f657e5f8ee8f37610d26d257ab8903a404a47
Waldemar Adams <waldek-adamski@wp.pl>
2020-05-08 01:20:28 +0200

    modify main.js

--- main.js
+++ main.js
@@ -4,12 +4,26 @@ const handHour = document.querySelector('div.hour');
 
 const nowTime = new Date();
 
-
-function getSeconds() {
+function setDate() {
     const nowTime = new Date();
+
     seconds = nowTime.getSeconds();
     secDeg = ((seconds / 60) * 360) + 90
     console.log(seconds);
-    handMinutes.style.transform = `rotate(${secDeg}deg)`
+    handSecond.style.transform = `rotate(${secDeg}deg)`
+
+    minutes = nowTime.getMinutes();
+    minDeg = ((minutes / 60) * 360) + 90
+    console.log(minutes);
+    handMinutes.style.transform = `rotate(${minDeg}deg)`
+
+
+    hour = nowTime.getHours();
+    hourDeg = ((hour / 24) * 360) + 90
+    console.log(hour);
+    handHour.style.transform = `rotate(${hourDeg}deg)`
+
+
 }
-setInterval(getSeconds, 1000)
+setInterval(setDate, 1000)
+setDate()
